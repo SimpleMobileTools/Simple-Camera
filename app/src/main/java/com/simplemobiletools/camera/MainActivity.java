b66ecf876963c11e6a699816389801f53b15f011
tibbi <tibor@kaputa.sk>
2016-04-13 20:44:03 +0200

    add Camera open / release

--- app/src/main/java/com/simplemobiletools/camera/MainActivity.java
+++ app/src/main/java/com/simplemobiletools/camera/MainActivity.java
@@ -1,13 +1,49 @@
 package com.simplemobiletools.camera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Camera camera;
+    private boolean isOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private void openCamera() {
+        isOpen = false;
+
+        try {
+            releaseCamera();
+            camera = Camera.open();
+            isOpen = (camera != null);
+        } catch (Exception e) {
+            Log.e(TAG, "openCamera exception " + e.getMessage());
+        }
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 }
