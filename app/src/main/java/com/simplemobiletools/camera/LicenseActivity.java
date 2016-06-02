9b681d95e2714af4b0d551bfa0f8fbc414fb9245
tibbi <tibor@kaputa.sk>
2016-06-02 19:24:36 +0200

    add licences

---
+++ app/src/main/java/com/simplemobiletools/camera/LicenseActivity.java
@@ -0,0 +1,28 @@
+package com.simplemobiletools.camera;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LicenseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.license_butterknife_title)
+    public void butterKnifeClicked() {
+        openUrl(getResources().getString(R.string.butterknife_url));
+    }
+
+    private void openUrl(String url) {
+        final Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+    }
+}
