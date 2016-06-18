b0d6641a54865226969bf176b9bebbb746eb51e0
tibbi <tibor@kaputa.sk>
2016-06-18 17:16:05 +0200

    add a Settings activity + allow using settings / about / license in landscape

---
+++ app/src/main/java/com/simplemobiletools/camera/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.simplemobiletools.camera;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+    }
+}
