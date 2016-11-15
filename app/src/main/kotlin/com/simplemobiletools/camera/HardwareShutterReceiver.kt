c50608aa2709df161ffe4cad019977c66b2ee949
tibbi <tibor@kaputa.sk>
2016-11-15 23:05:44 +0100

    convert HardwareShutterReceiver to kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/camera/HardwareShutterReceiver.kt
@@ -0,0 +1,17 @@
+package com.simplemobiletools.camera
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+import com.simplemobiletools.camera.activities.MainActivity
+
+class HardwareShutterReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Intent(context.applicationContext, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(this)
+        }
+    }
+}
